d9d8d37023998355543fe4d751623082e89c9271
NuttyShrimp <jan.lecoutere@hotmail.be>
2025-06-06 00:18:33 +0200

    feat(secret-sync): finalize syncing functions

--- backend/src/services/secret-sync/secret-sync-fns.ts
+++ backend/src/services/secret-sync/secret-sync-fns.ts
@@ -64,7 +64,7 @@ import { TERRAFORM_CLOUD_SYNC_LIST_OPTION, TerraformCloudSyncFns } from "./terra
 import { VERCEL_SYNC_LIST_OPTION, VercelSyncFns } from "./vercel";
 import { WINDMILL_SYNC_LIST_OPTION, WindmillSyncFns } from "./windmill";
 import { ZABBIX_SYNC_LIST_OPTION, ZabbixSyncFns } from "./zabbix";
-import { COOLIFY_SYNC_LIST_OPTION } from "./coolify";
+import { COOLIFY_SYNC_LIST_OPTION, CoolifySyncFns } from "./coolify";
 
 const SECRET_SYNC_LIST_OPTIONS: Record<SecretSync, TSecretSyncListItem> = {
   [SecretSync.AWSParameterStore]: AWS_PARAMETER_STORE_SYNC_LIST_OPTION,
@@ -439,6 +439,9 @@ export const SecretSyncFns = {
       case SecretSync.OctopusDeploy:
         secretMap = await OctopusDeploySyncFns.getSecrets(secretSync);
         break;
+      case SecretSync.Coolify:
+        secretMap = await CoolifySyncFns.getSecrets(secretSync);
+        break;
       default:
         throw new Error(
           `Unhandled sync destination for get secrets fns: ${(secretSync as TSecretSyncWithCredentials).destination}`
@@ -540,6 +543,8 @@ export const SecretSyncFns = {
         return ChefSyncFns.removeSecrets(secretSync, schemaSecretMap);
       case SecretSync.OctopusDeploy:
         return OctopusDeploySyncFns.removeSecrets(secretSync, schemaSecretMap);
+      case SecretSync.Coolify:
+        return CoolifySyncFns.removeSecrets(secretSync, schemaSecretMap);
       default:
         throw new Error(
           `Unhandled sync destination for remove secrets fns: ${(secretSync as TSecretSyncWithCredentials).destination}`
