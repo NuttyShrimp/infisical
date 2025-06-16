ec87c11806d3b3c992b3232a0d7e0f38d4ca3037
NuttyShrimp <jan.lecoutere@hotmail.be>
2025-06-16 15:21:20 +0200

    fix(frontend): add missing connection option type

--- frontend/src/hooks/api/appConnections/types/app-options.ts
+++ frontend/src/hooks/api/appConnections/types/app-options.ts
@@ -277,6 +277,10 @@ export type TDatadogConnectionOption = TAppConnectionOptionBase & {
   app: AppConnection.Datadog;
 };
 
+export type TCoolifyConnectionOption = TAppConnectionOptionBase & {
+  app: AppConnection.Coolify;
+};
+
 export type TAppConnectionOption =
   | TAwsConnectionOption
   | TGitHubConnectionOption
@@ -343,7 +347,8 @@ export type TAppConnectionOption =
   | TTravisCIConnectionOption
   | TSalesforceConnectionOption
   | TSnowflakeConnectionOption
-  | TDatadogConnectionOption;
+  | TDatadogConnectionOption
+  | TCoolifyConnectionOption;
 
 export type TAppConnectionOptionMap = {
   [AppConnection.AWS]: TAwsConnectionOption;
@@ -412,4 +417,5 @@ export type TAppConnectionOptionMap = {
   [AppConnection.Salesforce]: TSalesforceConnectionOption;
   [AppConnection.Snowflake]: TSnowflakeConnectionOption;
   [AppConnection.Datadog]: TDatadogConnectionOption;
+  [AppConnection.Coolify]: TCoolifyConnectionOption;
 };
